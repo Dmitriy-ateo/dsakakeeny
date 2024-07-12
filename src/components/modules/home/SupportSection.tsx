330b9805c07e96d0c19375092e8050963051e856
Dmitry Kabakov - CW <dmitriy.ateo@gmail.com>
2024-07-12 15:51:03 +0200

    Fix swiper

--- src/components/modules/home/SupportSection.tsx
+++ src/components/modules/home/SupportSection.tsx
@@ -12,7 +12,7 @@ import {
 
 const SupportSection = () => (
   <section className="flex flex-col flex-nowrap container px-4 py-8 items-center sm:items-start gap-16 sm:flex-row">
-    <div className="shrink">
+    <div className="shrink min-w-0 md:min-w-[300px]">
       <Image
         src="/assets/images/chess.png"
         alt="Chef Problem Solver"
@@ -21,37 +21,39 @@ const SupportSection = () => (
       />
     </div>
 
-    <div className="flex flex-col">
+    <div className="flex flex-col min-w-0 max-w-full">
       <p className="uppercase text-xs">Strategic Partner</p>
       <p className="text-3xl sm:text-5xl font-bold mt-4 mb-8">
         Driving Your Success
       </p>
 
-      <Swiper
-        className="max-w-[350px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[600px] xl:max-w-[800px] 2xl:max-w-[1000px] w-full mb-8"
-        slidesPerView={1}
-        modules={[Pagination]}
-        loop
-        pagination={{ clickable: true }}
-        autoHeight
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-      >
-        {HOW_CAN_I_SUPPORT_ITEMS.map((p) => {
-          return (
-            <SwiperSlide
-              className="flex grow-0 shrink pb-8 min-h-[300px] sm:min-h-[200px]"
-              key={`support_${p.id}`}
-            >
-              <p className="font-bold mb-2">{p.header}</p>
-              <p className="text-base mb-2">{p.info}</p>
-              <p className="text-base mb-2">{p.additional}</p>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      <div className="block min-w-0 max-w-full">
+        <Swiper
+          className="w-full mb-8"
+          slidesPerView={1}
+          modules={[Pagination]}
+          loop
+          pagination={{ clickable: true }}
+          autoHeight
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          {HOW_CAN_I_SUPPORT_ITEMS.map((p) => {
+            return (
+              <SwiperSlide
+                className="flex grow-0 shrink pb-8 min-h-[300px] sm:min-h-[200px]"
+                key={`support_${p.id}`}
+              >
+                <p className="font-bold mb-2">{p.header}</p>
+                <p className="text-base mb-2">{p.info}</p>
+                <p className="text-base mb-2">{p.additional}</p>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
 
       <div className="experience_wrapper">
         {EXPERIENCE_ITEMS.map((p) => {
--- src/components/modules/testimonials.tsx
+++ src/components/modules/testimonials.tsx
@@ -13,48 +13,50 @@ const TestimonialSection = () => (
     <p className="text-5xl font-bold mb-4">Kudus, Denice</p>
     <p className="text-base mb-8"> A few kind words from my clients</p>
 
-    <Swiper
-      className="max-w-[300px] sm:max-w-[450px] md:max-w-[600px] lg:max-w-[800px] xl:max-w-[1000px] 2xl:max-w-[1200px] w-full mb-8"
-      slidesPerView={1}
-      spaceBetween={10}
-      breakpoints={{
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        960: {
-          slidesPerView: 2,
-          spaceBetween: 40,
-        },
-        1240: {
-          slidesPerView: 3,
-          spaceBetween: 50,
-        },
-      }}
-      modules={[Pagination]}
-      loop
-      pagination={{ clickable: true }}
-      autoHeight
-      autoplay={{
-        delay: 1000,
-        disableOnInteraction: false,
-      }}
-    >
-      {TESTIMONIALS.map((p) => {
-        return (
-          <SwiperSlide
-            className="flex grow-0 shrink pb-10 min-h-[300px]"
-            key={`slide_${p.id}`}
-          >
-            <p className="text-base mb-2">{p.testimonial}</p>
-            <a className="text-xs font-bold mb-1" href={p.companyLink}>
-              {p.company}
-            </a>
-            <p className="text-xs">{p.title}</p>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <div className="block min-w-0 max-w-full">
+      <Swiper
+        className="w-full mb-8"
+        slidesPerView={1}
+        spaceBetween={10}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          960: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1240: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        loop
+        pagination={{ clickable: true }}
+        autoHeight
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+      >
+        {TESTIMONIALS.map((p) => {
+          return (
+            <SwiperSlide
+              className="flex grow-0 shrink pb-10 min-h-[300px]"
+              key={`slide_${p.id}`}
+            >
+              <p className="text-base mb-2">{p.testimonial}</p>
+              <a className="text-xs font-bold mb-1" href={p.companyLink}>
+                {p.company}
+              </a>
+              <p className="text-xs">{p.title}</p>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   </section>
 );
 
